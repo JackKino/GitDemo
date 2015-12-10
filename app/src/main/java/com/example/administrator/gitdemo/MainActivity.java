e01898525d65caef1b2f5adbf58a5a8b5159abef
JackKino <jackchen9408@sina.com>
2015-12-10 11:30:48 +0800

    first

--- app/src/main/java/com/example/administrator/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/gitdemo/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+//修改1
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
